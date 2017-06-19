03dbd34ae39b165a6aa453656d635fd568092141
Iivo Raitahila <iivo.raitahila@helsinki.fi>
2017-06-19 15:59:20 +0300

    progress

--- src/main/java/com/mulperi/controller/SubmitController.java
+++ src/main/java/com/mulperi/controller/SubmitController.java
@@ -1,20 +1,45 @@
 package com.mulperi.controller;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mulperi.models.ParsedModel;
+import com.mulperi.models.submit.Relationship;
+import com.mulperi.models.submit.Requirement;
+import com.mulperi.services.FormatTransformerService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
-@Controller
+@RestController
+@RequestMapping("submit")
 public class SubmitController {
 
-	@RequestMapping(value = "/submitModel", method = RequestMethod.POST)
-    @ResponseBody
-    public String home() {
+	private FormatTransformerService transform = new FormatTransformerService();
+	
+	@RequestMapping(value = "/simple", method = RequestMethod.POST)
+    public Requirement simpleIn(@RequestBody Requirement req) {
+		ParsedModel pm = transform.SimpleToKumbang(req);
 		
-        return "TBD";
+        return req;
+    }
+	
+	
+	@RequestMapping(value = "", method = RequestMethod.GET)
+    public Requirement test() { //@RequestParam String content
+		Requirement koe = new Requirement();
+		koe.setId("Koe");
+		Relationship yksi = new Relationship();
+		yksi.setId("koe");
+		yksi.setType("isa");
+		List<Relationship> relationships = new ArrayList<Relationship>();
+		relationships.add(yksi);
+		koe.setRelationships(relationships);
+        return koe;
     }
 
 }
