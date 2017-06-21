7647414aeed482448fba5e1f60f96d00c07f141b
Iivo Raitahila <iivo.raitahila@helsinki.fi>
2017-06-21 16:13:32 +0300

    added HTTP codes and error messages

--- src/main/java/com/mulperi/services/CaasClient.java
+++ src/main/java/com/mulperi/services/CaasClient.java
@@ -2,7 +2,9 @@ package com.mulperi.services;
 
 import java.io.StringWriter;
 import java.util.ArrayList;
+import java.util.zip.DataFormatException;
 
+import javax.management.IntrospectionException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -15,51 +17,72 @@ import javax.xml.transform.stream.StreamResult;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
 
 import com.mulperi.models.Configuration;
 import com.mulperi.models.Selection;
 
 public class CaasClient {
 
-	public String uploadConfigurationModel(String modelName, String model, String caasAddress)
-			throws ParserConfigurationException, TransformerException {
+	public String uploadConfigurationModel(String modelName, String model, String caasAddress) 
+			throws Exception, XMLStreamException, IntrospectionException, DataFormatException {
+		
 		RestTemplate rt = new RestTemplate();
-		
-		String result = "nothing";
-		
+
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_XML);
-		
+
 		//String hack = "<xml><model name=\""+ name + "\">" + model + "</model></xml>";
-		
-		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder dBuilder= dbFactory.newDocumentBuilder();
-		Document doc = dBuilder.newDocument();
-				
-		Element rootElement = doc.createElement("xml");
-		doc.appendChild(rootElement);
-		Element modelElement = doc.createElement("model");
-		rootElement.appendChild(modelElement);
-		
-		Attr attr = doc.createAttribute("name");
-		attr.setValue(modelName);
-		modelElement.setAttributeNode(attr);
 
-		modelElement.appendChild(doc.createTextNode(model));
+		StringWriter stringWriter = new StringWriter();
+		XMLOutputFactory xMLOutputFactory = XMLOutputFactory.newInstance();	
+		XMLStreamWriter xMLStreamWriter = xMLOutputFactory.createXMLStreamWriter(stringWriter);
+
+		xMLStreamWriter.writeStartDocument();
+		xMLStreamWriter.writeStartElement("xml");
+		xMLStreamWriter.writeStartElement("model");			
+		xMLStreamWriter.writeAttribute("name", modelName);
+		xMLStreamWriter.writeCharacters(model);
+		xMLStreamWriter.writeEndElement();
+		xMLStreamWriter.writeEndDocument();
+
+		xMLStreamWriter.flush();
+		xMLStreamWriter.close();
+		String xmlString = stringWriter.getBuffer().toString();
 		
-		HttpEntity<String> entity = new HttpEntity<String>(getStringFromDocument(doc), headers);
-
-		System.out.println(entity.toString());
-		ResponseEntity<String> response = rt.postForEntity(caasAddress, entity, String.class);
-		result = response.toString();
-		System.out.println(result.toString());
-
+		HttpEntity<String> entity = new HttpEntity<String>(xmlString, headers);
+		
+		ResponseEntity<String> response = null;
+		try {
+			response = rt.postForEntity(caasAddress, entity, String.class);
+		} catch (HttpServerErrorException e) {
+			
+			if(e.getStatusCode() == HttpStatus.INTERNAL_SERVER_ERROR 
+					&& e.getResponseBodyAsString().contains("There are no configurations that satisfy the given model.")) {
+				throw new IntrospectionException(e.getResponseBodyAsString());
+			}
+			if(e.getStatusCode() == HttpStatus.INTERNAL_SERVER_ERROR 
+					&& e.getResponseBodyAsString().contains("Parsing of model failed.")) {
+				throw new DataFormatException(e.getResponseBodyAsString());
+			}
+			if(e.getStatusCode() != HttpStatus.CREATED) {
+				throw new Exception(e.getResponseBodyAsString());
+			}
+		}
+		
+		String result = response.toString();
+		System.out.println(result);
 		return result;
 
 	}
