5ce969ebc4d74e74420f8a9cef56ea7ddaeff901
Myllyaho Lalli S <lallimyl@hp8x-24.cs.helsinki.fi>
2018-07-18 16:06:41 +0300

    minor fix regarding attributes' source

--- src/main/java/eu/openreq/mulperi/services/MurmeliModelGenerator.java
+++ src/main/java/eu/openreq/mulperi/services/MurmeliModelGenerator.java
@@ -315,6 +315,7 @@ public class MurmeliModelGenerator {
 		
 		String name = req.getId();
 		AttributeValue<Integer> priority = new AttributeValue("priority", true, req.getPriority());
+		priority.setSource(Source.FIXED);
 		priority.setType(this.attributeValueTypes.get("priority"));
 		
 		this.attributeValues.put(priority.getID(), priority);
