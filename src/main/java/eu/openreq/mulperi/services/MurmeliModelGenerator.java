130f881f59d8f77f9b9d7feb489c10b9ceb659f9
Myllyaho Lalli S <lallimyl@hp8x-24.cs.helsinki.fi>
2018-10-31 17:26:13 +0200

    added handling for some requirementparts in murmelimodelgenerator. untested!

--- src/main/java/eu/openreq/mulperi/services/MurmeliModelGenerator.java
+++ src/main/java/eu/openreq/mulperi/services/MurmeliModelGenerator.java
@@ -25,6 +25,11 @@ public class MurmeliModelGenerator {
 	private List<Container> subContainers;
 	private HashSet<String> requirementsInReleases;
 	private HashMap<String, AttributeValue> resolutions;
+	private HashMap<String, AttributeValue> platforms;
+	private HashMap<String, AttributeValue> versions;
+	private HashMap<String, AttributeValue> environments;
+	private HashMap<String, AttributeValue> labels;
+	private HashMap<String, AttributeValue> fixVersions;
 	
 	public MurmeliModelGenerator() {
 		
@@ -37,6 +42,11 @@ public class MurmeliModelGenerator {
 		this.attributeValues = new HashMap();
 		this.subContainers = new ArrayList();
 		this.resolutions = new HashMap();
+		this.platforms = new HashMap<String, AttributeValue>();
+		this.versions = new HashMap<String, AttributeValue>();
+		this.environments = new HashMap<String, AttributeValue>();
+		this.labels = new HashMap<String, AttributeValue>();
+		this.fixVersions = new HashMap<String, AttributeValue>();
 		
 		this.requirementsInReleases = new HashSet();
 		
@@ -501,6 +511,56 @@ public class MurmeliModelGenerator {
 					}
 	
 					element.addAttribute(this.resolutions.get(part.getText()));
+					break;
+				} else if (part.getName().equals("Platform")) {
+					if (!this.platforms.containsKey(part.getText())) {
+						
+						AttributeValue atr = new AttributeValue("platform", false, part.getText());
+						this.resolutions.put(part.getText(), atr);
+						this.attributeValues.put(atr.getID(), atr);
+					}
+					
+					element.addAttribute(this.platforms.get(part.getText()));
+					break;
+				} else if (part.getName().equals("Version")) {
+					if (!this.versions.containsKey(part.getText())) {
+						
+						AttributeValue atr = new AttributeValue("version", false, part.getText());
+						this.versions.put(part.getText(), atr);
+						this.attributeValues.put(atr.getID(), atr);
+					}
+					
+					element.addAttribute(this.versions.get(part.getText()));
+					break;
+				} else if (part.getName().equals("Labels")) {
+					if (!this.labels.containsKey(part.getText())) {
+						
+						AttributeValue atr = new AttributeValue("label", false, part.getText());
+						this.labels.put(part.getText(), atr);
+						this.attributeValues.put(atr.getID(), atr);
+					}
+					
+					element.addAttribute(this.labels.get(part.getText()));
+					break;
+				} else if (part.getName().equals("Environment")) {
+					if (!this.environments.containsKey(part.getText())) {
+						
+						AttributeValue atr = new AttributeValue("environment", false, part.getText());
+						this.environments.put(part.getText(), atr);
+						this.attributeValues.put(atr.getID(), atr);
+					}
+					
+					element.addAttribute(this.environments.get(part.getText()));
+					break;
+				} else if (part.getName().equals("FixVersion")) {
+					if (!this.fixVersions.containsKey(part.getText())) {
+						
+						AttributeValue atr = new AttributeValue("fixVersion", false, part.getText());
+						this.fixVersions.put(part.getText(), atr);
+						this.attributeValues.put(atr.getID(), atr);
+					}
+					
+					element.addAttribute(this.fixVersions.get(part.getText()));
 					break;
 				}
 			}
