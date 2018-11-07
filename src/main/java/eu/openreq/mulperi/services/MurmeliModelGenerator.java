2e8da161f7d58b6d733f67baba734ad2aabf9113
Myllyaho Lalli S <lallimyl@hp8x-24.cs.helsinki.fi>
2018-11-07 16:04:54 +0200

    functions to get RequirementParts to Murmeli and back to OpenReq

--- src/main/java/eu/openreq/mulperi/services/MurmeliModelGenerator.java
+++ src/main/java/eu/openreq/mulperi/services/MurmeliModelGenerator.java
@@ -30,6 +30,7 @@ public class MurmeliModelGenerator {
 	private HashMap<String, AttributeValue> environments;
 	private HashMap<String, AttributeValue> labels;
 	private HashMap<String, AttributeValue> fixVersions;
+	private HashMap<String, AttributeValue> components;
 	
 	public MurmeliModelGenerator() {
 		
@@ -47,6 +48,7 @@ public class MurmeliModelGenerator {
 		this.environments = new HashMap<String, AttributeValue>();
 		this.labels = new HashMap<String, AttributeValue>();
 		this.fixVersions = new HashMap<String, AttributeValue>();
+		this.components = new HashMap<String, AttributeValue>();
 		
 		this.requirementsInReleases = new HashSet();
 		
@@ -501,7 +503,7 @@ public class MurmeliModelGenerator {
 	 */
 	private void resolutionToElement(Requirement req, Element element) {
 		if(req.getRequirementParts()!=null) {
-			for (RequirementPart part : req.getRequirementParts()) {
+			for (RequirementPart part : req.getRequirementParts()) {				
 				if (part.getName().equals("Resolution")) {
 					if (!this.resolutions.containsKey(part.getText())) {
 						
@@ -515,7 +517,7 @@ public class MurmeliModelGenerator {
 				} else if (part.getName().equals("Platforms")) {
 					if (!this.platforms.containsKey(part.getText())) {
 						
-						AttributeValue atr = new AttributeValue("platform", false, part.getText());
+						AttributeValue atr = new AttributeValue("platforms", false, part.getText());
 						this.platforms.put(part.getText(), atr);
 						this.attributeValues.put(atr.getID(), atr);
 					}
@@ -525,7 +527,7 @@ public class MurmeliModelGenerator {
 				} else if (part.getName().equals("Versions")) {
 					if (!this.versions.containsKey(part.getText())) {
 						
-						AttributeValue atr = new AttributeValue("version", false, part.getText());
+						AttributeValue atr = new AttributeValue("versions", false, part.getText());
 						this.versions.put(part.getText(), atr);
 						this.attributeValues.put(atr.getID(), atr);
 					}
@@ -535,7 +537,7 @@ public class MurmeliModelGenerator {
 				} else if (part.getName().equals("Labels")) {
 					if (!this.labels.containsKey(part.getText())) {
 						
-						AttributeValue atr = new AttributeValue("label", false, part.getText());
+						AttributeValue atr = new AttributeValue("labels", false, part.getText());
 						this.labels.put(part.getText(), atr);
 						this.attributeValues.put(atr.getID(), atr);
 					}
@@ -561,6 +563,16 @@ public class MurmeliModelGenerator {
 					}
 					
 					element.addAttribute(this.fixVersions.get(part.getText()));
+//					break;
+				} else if (part.getName().equals("Components")) {
+					if (!this.components.containsKey(part.getText())) {
+						
+						AttributeValue atr = new AttributeValue("components", false, part.getText());
+						this.components.put(part.getText(), atr);
+						this.attributeValues.put(atr.getID(), atr);
+					}
+					
+					element.addAttribute(this.components.get(part.getText()));
 //					break;
 				}
 				//Add also Components here! 
