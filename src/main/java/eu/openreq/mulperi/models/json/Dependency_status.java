ddffe20e87803ba01ed0201a9d01fbc2c67fbb68
Kettunen <ejkettun@lm7-cs-41.pc.helsinki.fi>
2018-11-07 15:18:52 +0200

    fixing enums

--- src/main/java/eu/openreq/mulperi/models/json/Dependency_status.java
+++ src/main/java/eu/openreq/mulperi/models/json/Dependency_status.java
@@ -1,9 +1,26 @@
 package eu.openreq.mulperi.models.json;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.gson.annotations.SerializedName;
+
 public enum Dependency_status {
 
+//	PROPOSED,
+//	ACCEPTED,
+//	REJECTED
+//	
+	@SerializedName(value="proposed", alternate= {"PROPOSED"})
 	PROPOSED,
+	@SerializedName(value="accepted", alternate= {"ACCEPTED"})
 	ACCEPTED,
+	@SerializedName(value="rejected", alternate= {"REJECTED"})
 	REJECTED
+	
+//	@JsonProperty("proposed")
+//	PROPOSED,
+//	@JsonProperty("accepted")
+//	ACCEPTED,
+//	@JsonProperty("rejected")
+//	REJECTED
 	
 }
--- src/main/java/eu/openreq/mulperi/models/json/Dependency_type.java
+++ src/main/java/eu/openreq/mulperi/models/json/Dependency_type.java
@@ -1,15 +1,56 @@
 package eu.openreq.mulperi.models.json;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.gson.annotations.SerializedName;
+
 public enum Dependency_type {
 
+//	CONTRIBUTES,
+//	DAMAGES,
+//	REFINES,
+//	REQUIRES,
+//	INCOMPATIBLE,
+//	DECOMPOSITION,
+//	SIMILAR,
+//	DUPLICATES,
+//	REPLACES
+	
+	@SerializedName(value="contributes", alternate= {"CONTRIBUTES"})
 	CONTRIBUTES,
+	@SerializedName(value="damages", alternate= {"DAMAGES"})
 	DAMAGES,
+	@SerializedName(value="refines", alternate= {"REFINES"})
 	REFINES,
+	@SerializedName(value="requires", alternate= {"REQUIRES"})
 	REQUIRES,
+	@SerializedName(value="incompatible", alternate= {"INCOMPATIBLE"})
 	INCOMPATIBLE,
+	@SerializedName(value="decomposition", alternate= {"DECOMPOSITION"})
 	DECOMPOSITION,
+	@SerializedName(value="similar", alternate= {"SIMILAR"})
 	SIMILAR,
+	@SerializedName(value="duplicates", alternate= {"DUPLICATES"})
 	DUPLICATES,
+	@SerializedName(value="replaces", alternate= {"REPLACES"})
 	REPLACES
+	
+//	@JsonProperty("contributes")
+//	CONTRIBUTES,
+//	@JsonProperty("damages")
+//	DAMAGES,
+//	@JsonProperty("refines")
+//	REFINES,
+//	@JsonProperty("requires")
+//	REQUIRES,
+//	@JsonProperty("incompatible")
+//	INCOMPATIBLE,
+//	@JsonProperty("decomposition")
+//	DECOMPOSITION,
+//	@JsonProperty("similar")
+//	SIMILAR,
+//	@JsonProperty("duplicates")
+//	DUPLICATES,
+//	@JsonProperty("replaces")
+//	REPLACES
 
 }
