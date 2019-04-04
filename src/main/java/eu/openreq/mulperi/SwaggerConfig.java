77eeb54b394ae3c34995e503bd3239610e7e3a86
laurtom <tlaurinen@gmail.com>
2019-04-04 12:32:27 +0300

    config changed

--- src/main/java/eu/openreq/mulperi/SwaggerConfig.java
+++ src/main/java/eu/openreq/mulperi/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig {
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-        		//.host("api.openreq.eu/mulperi")
+        		.host("api.openreq.eu/mulperi")
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("eu.openreq.mulperi.controllers"))    
                 .paths(PathSelectors.any())  
